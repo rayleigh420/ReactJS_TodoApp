c92f4e8abcb156b11ce003d60dae2b348cfbbe0c
Duy Le <nhatduy0409@gmail.com>
2023-01-25 03:46:58 +0000

    Add json-server and create form todo

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,17 +1,30 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import axios from "axios";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(123);
+    let result = await axios.get("https://z0o0wo-3500.preview.csb.app/todos");
+    console.log(result.data);
+  };
 
   const newItemSection = (
     <form>
       <label htmlFor="new-todo">Enter a new todo item</label>
       <div className="new-todo">
-        <input type="text" id="new-todo" placeholder="Enter new todo" />
+        <input
+          type="text"
+          id="new-todo"
+          placeholder="Enter new todo"
+          value="Hello"
+        />
       </div>
-      <button className="submit">
+      <button className="submit" onClick={handleSubmit}>
         <FontAwesomeIcon icon={faUpload} />
       </button>
     </form>
