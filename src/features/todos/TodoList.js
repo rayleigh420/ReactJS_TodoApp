f7885e6c76095f7f4e852a0e1d723bd23dc8e825
Duy Le <nhatduy0409@gmail.com>
2023-01-25 03:53:52 +0000

    fix something

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -5,26 +5,28 @@ import axios from "axios";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
+  const [todo, setTodo] = useState("hello");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(123);
     let result = await axios.get("https://z0o0wo-3500.preview.csb.app/todos");
     console.log(result.data);
+    setTodo(result.data[0].title);
   };
 
   const newItemSection = (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="new-todo">Enter a new todo item</label>
       <div className="new-todo">
         <input
           type="text"
           id="new-todo"
           placeholder="Enter new todo"
-          value="Hello"
+          value={newTodo}
         />
       </div>
-      <button className="submit" onClick={handleSubmit}>
+      <button className="submit">
         <FontAwesomeIcon icon={faUpload} />
       </button>
     </form>
@@ -34,6 +36,15 @@ const TodoList = () => {
     <main>
       <h1>Todo List</h1>
       {newItemSection}
+      <div>{todo}</div>
+      {/* {todo.map((item, index) => {
+        <div key={index}>{item.title}</div>;
+      })} */}
+      {/* {todo ? (
+        
+      ) : (
+        <div>Hello</div>
+      )} */}
     </main>
   );
 };
